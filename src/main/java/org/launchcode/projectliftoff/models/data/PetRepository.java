2f702312fdada2b9749e9df5fa352f9ac7ecdcbc
Katlyn McDonald <kkmcoding@gmail.com>
2022-11-10 02:41:16 -0500

    experimenting(created nav bar, added project files, etc)

---
+++ src/main/java/org/launchcode/projectliftoff/models/data/PetRepository.java
@@ -0,0 +1,11 @@
+package org.launchcode.projectliftoff.models.data;
+
+import org.launchcode.projectliftoff.models.Dog;
+import org.launchcode.projectliftoff.models.Cat;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PetRepository <Name, Species> {
+
+}
