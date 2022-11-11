ff74eef4537aa0c4eafc9e16c9ece813cd0c70c7
mahdi <mahdibelabbas@gmail.com>
2022-11-10 20:05:04 -0500

    home page html and css  design

--- src/main/java/org/launchcode/projectliftoff/controllers/HelloController.java
+++ src/main/java/org/launchcode/projectliftoff/controllers/HelloController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+
 public class HelloController {
 
-    @RequestMapping(value = "")
-    @ResponseBody //returns plain text, remove after adding templates
-    public String index() {
-        return "Hello, world!";
-    }
+//    @RequestMapping(value = "")
+//    @ResponseBody //returns plain text, remove after adding templates
+//    public String index() {
+//        return "Hello, world!";
+//    }
 }
