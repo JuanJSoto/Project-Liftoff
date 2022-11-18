1a58b44bbc9866c44b5a3db878a5ae17575ff61d
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2022-11-17 19:23:33 -0500

    created classes updat

---
+++ src/main/java/org/launchcode/projectliftoff/models/AbstractEntity.java
@@ -0,0 +1,29 @@
+package org.launchcode.projectliftoff.models;
+
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+public class AbstractEntity {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/org/launchcode/projectliftoff/models/Dog.java
@@ -0,0 +1,43 @@
+package org.launchcode.projectliftoff.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dog extends Animals {
+    private Boolean trained;
+
+      private List<Dog> dogs= new ArrayList<>();
+
+    public List<Dog> getDogs() {
+        return dogs;
+    }
+
+    public void setDogs(List<Dog> dogs) {
+        this.dogs = dogs;
+    }
+
+    public Dog(String name, int age, String color, String breed, String location, Boolean trained) {
+        super(name, age, color, breed, location);
+        this.trained = trained;
+    }
+
+
+    public Boolean getTrained() {
+        return trained;
+    }
+
+    public void setTrained(Boolean trained) {
+        this.trained = trained;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "name= " + name + "\n" +
+                        "color= " + color + "\n" +
+                        "breed= " + breed + "\n" +
+                        "age= " + age +
+                        "trained" + trained;
+    }
+
+}
