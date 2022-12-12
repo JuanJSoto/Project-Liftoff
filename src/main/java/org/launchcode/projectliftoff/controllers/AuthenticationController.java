a6340818c5fd6c34f899b40c8daf1b927f96becd
wsophilly <josephfsimmonsjr@gmail.com>
2022-12-11 21:01:24 -0500

    updated the application properties. and added the login form

--- src/main/java/org/launchcode/projectliftoff/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/projectliftoff/controllers/AuthenticationController.java
@@ -2,12 +2,18 @@ package org.launchcode.projectliftoff.controllers;
 
 import org.launchcode.projectliftoff.data.UserRepository;
 import org.launchcode.projectliftoff.models.User;
+import org.launchcode.projectliftoff.models.dto.LoginFormDTO;
 import org.launchcode.projectliftoff.models.dto.RegisterFormDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -36,11 +42,50 @@ public class AuthenticationController {
         session.setAttribute(userSessionKey, user.getId());
     }
 
-    @GetMapping("/register")
+    @GetMapping("/login-register")
     public String displayRegistrationForm(Model model) {
         model.addAttribute("registerFormDTO",new RegisterFormDTO());
         model.addAttribute("title", "Register User");
-        return "register";
+        return "login-register";
     }
 
+    @PostMapping("/login-register")
+    public String processRegistrationForm(@ModelAttribute @Valid RegisterFormDTO registerFormDTO,
+                                          Errors errors, HttpServletRequest request,
+                                          Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Register");
+            return "login-register";
+        }
+
+        User existingUser = userRepository.findByUsername(registerFormDTO.getUsername());
+
+        if (existingUser != null) {
+            errors.rejectValue("username", "username.alreadyexists", "A user with that username already exists");
+            model.addAttribute("title", "Register");
+            return "login-register";
+        }
+
+        String password = registerFormDTO.getPassword();
+        String verifyPassword = registerFormDTO.getVerifyPassword();
+        if (!password.equals(verifyPassword)) {
+            errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
+            model.addAttribute("title", "Register");
+            return "login-register";
+        }
+
+        User newUser = new User(registerFormDTO.getUsername(), registerFormDTO.getPassword());
+        userRepository.save(newUser);
+        setUserInSession(request.getSession(), newUser);
+
+        return "redirect:";
+    }
+
+    @GetMapping("/login")
+    public String displayLoginForm(Model model) {
+        model.addAttribute("loginFormDTO",new LoginFormDTO());
+        model.addAttribute("title", "Log In");
+        return "login";
+    }
 }
