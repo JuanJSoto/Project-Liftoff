f1a2b1a44fd2e427859fd01a38c9ca0fad981b87
Katlyn McDonald <kkmcoding@gmail.com>
2022-12-12 16:40:44 -0500

    pet details page done, still need to connect api to traits and photo

--- src/main/java/org/launchcode/projectliftoff/controllers/PetDetailController.java
+++ src/main/java/org/launchcode/projectliftoff/controllers/PetDetailController.java
@@ -1,4 +1,15 @@
 package org.launchcode.projectliftoff.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PetDetailController {
+
+    @GetMapping("pet-details")
+    public String petDetails(Model model) {
+        return "pet-details";
+    }
+
 }
