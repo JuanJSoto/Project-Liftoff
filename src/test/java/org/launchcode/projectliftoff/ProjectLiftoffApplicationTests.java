8b096f70556becab97a2627347e2aa1db494160d
Juan J Soto <jj3185@hotmail.com>
2023-03-20 02:59:34 -0400

    hid coding within application tests

--- src/test/java/org/launchcode/projectliftoff/ProjectLiftoffApplicationTests.java
+++ src/test/java/org/launchcode/projectliftoff/ProjectLiftoffApplicationTests.java
@@ -1,13 +1,13 @@
-package org.launchcode.projectliftoff;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProjectLiftoffApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package org.launchcode.projectliftoff;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProjectLiftoffApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
