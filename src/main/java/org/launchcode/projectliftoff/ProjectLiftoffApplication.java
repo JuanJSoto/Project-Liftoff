368ff92ac0b44332a23ac9419a6ce71dbac9272e
Juan J Soto <jj3185@hotmail.com>
2023-05-08 14:25:06 -0400

    Update package json and ProjectLiftOff

--- src/main/java/org/launchcode/projectliftoff/ProjectLiftoffApplication.java
+++ src/main/java/org/launchcode/projectliftoff/ProjectLiftoffApplication.java
@@ -3,9 +3,8 @@ package org.launchcode.projectliftoff;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
 @EnableAutoConfiguration
 public class ProjectLiftoffApplication {
 
