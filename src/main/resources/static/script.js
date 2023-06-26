d440f0fe19b715b10919266603b6d2a6779d26d7
Juan J Soto <jj3185@hotmail.com>
2023-06-25 21:57:23 -0400

    deleted commented lines out of out/production/resources/static/script.js that were same earlier issues

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -181,16 +181,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function initMap() {
   geocoder = new google.maps.Geocoder();
-  // The location of Uluru
   const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: uluru,
   });
-  // The marker, positioned at Uluru
-//  const marker = new google.maps.Marker({
-//    position: uluru,
-//    map: map,
-//  });
 }
